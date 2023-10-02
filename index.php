2f8b6a33d9514586253bd4a29aa7d6e3dfedbb33
Brandon Uriel solano Moreno <brandonmoreno2030@gmail.com>
2023-10-02 10:46:56 -0600

    version2 de alertSystem

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AlertSystem</title>
+    <?php
+    include "src/views/head.php";
+    ?>
+</head>
+<body>
+<?php
+    include "src/views/search.php";
+    ?>
+    <?php
+    include "src/views/dashboard.php";
+    ?>
+
+<?php
+    include "src/views/script.php";
+    ?>
+</body>
+</html>
---
+++ src/views/dashboard.php
@@ -0,0 +1,124 @@
+<div class="container-scroller">
+    
+    <!-- partial:partials/_sidebar.html -->
+    <nav class="sidebar sidebar-offcanvas" id="sidebar">
+      <ul class="nav">
+        <li class="nav-item">
+          <a class="nav-link" href="index.html">
+            <i class="icon-grid menu-icon"></i>
+            <span class="menu-title">Inicio</span>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" data-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
+            <i class="icon-layout menu-icon"></i>
+            <span class="menu-title">Contratacion</span>
+            <i class="menu-arrow"></i>
+          </a>
+          <div class="collapse" id="ui-basic">
+            <ul class="nav flex-column sub-menu">
+              <li class="nav-item"> <a class="nav-link" href="solEmpleo.html">Solicitud de Empleo</a></li>
+            </ul>
+          </div>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" data-toggle="collapse" href="#form-elements" aria-expanded="false" aria-controls="form-elements">
+            <i class="icon-columns menu-icon"></i>
+            <span class="menu-title">Vacantes</span>
+            <i class="menu-arrow"></i>
+          </a>
+          <div class="collapse" id="form-elements">
+            <ul class="nav flex-column sub-menu">
+              <li class="nav-item"><a class="nav-link" href="pages/forms/basic_elements.html">Basic Elements</a></li>
+            </ul>
+          </div>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" data-toggle="collapse" href="#personal" aria-expanded="false" aria-controls="personal">
+            <i class="icon-head menu-icon"></i>
+            <span class="menu-title">Empleados</span>
+            <i class="menu-arrow"></i>
+          </a>
+          <div class="collapse" id="personal">
+            <ul class="nav flex-column sub-menu">
+              <li class="nav-item"> <a class="nav-link" href="php/personal.php">General</a></li>
+              <li class="nav-item"> <a class="nav-link" href="pages/charts/chartjs.html">Operarios</a></li>
+              <li class="nav-item"> <a class="nav-link" href="pages/charts/chartjs.html">Supervisores</a></li>
+              <li class="nav-item"> <a class="nav-link" href="pages/charts/chartjs.html">Analistas</a></li>
+              <li class="nav-item"> <a class="nav-link" href="pages/charts/chartjs.html">Almacenistas</a></li>
+            
+            
+            </ul>
+          </div>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" data-toggle="collapse" href="#tables" aria-expanded="false" aria-controls="tables">
+            <i class="icon-grid-2 menu-icon"></i>
+            <span class="menu-title">Capacitacion</span>
+            <i class="menu-arrow"></i>
+          </a>
+          <div class="collapse" id="tables">
+            <ul class="nav flex-column sub-menu">
+              <li class="nav-item"> <a class="nav-link" href="pages/tables/basic-table.html">Basic table</a></li>
+            </ul>
+          </div>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" data-toggle="collapse" href="#icons" aria-expanded="false" aria-controls="icons">
+            <i class="icon-bar-graph menu-icon"></i>
+            <span class="menu-title">Nominas</span>
+            <i class="menu-arrow"></i>
+          </a>
+          <div class="collapse" id="icons">
+            <ul class="nav flex-column sub-menu">
+              <li class="nav-item"> <a class="nav-link" href="pages/icons/mdi.html">Mdi icons</a></li>
+            </ul>
+          </div>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" data-toggle="collapse" href="#auth" aria-expanded="false" aria-controls="auth">
+            <i class="icon-contract menu-icon"></i>
+            <span class="menu-title">Costos</span>
+            <i class="menu-arrow"></i>
+          </a>
+          <div class="collapse" id="auth">
+            <ul class="nav flex-column sub-menu">
+              <li class="nav-item"> <a class="nav-link" href="pages/samples/login.html"> Login </a></li>
+              <li class="nav-item"> <a class="nav-link" href="pages/samples/register.html"> Register </a></li>
+            </ul>
+          </div>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" data-toggle="collapse" href="#error" aria-expanded="false" aria-controls="error">
+            <i class="icon-ban menu-icon"></i>
+            <span class="menu-title">Precio de venta por modelo</span>
+            <i class="menu-arrow"></i>
+          </a>
+          <div class="collapse" id="error">
+            <ul class="nav flex-column sub-menu">
+              <li class="nav-item"> <a class="nav-link" href="pages/samples/error-404.html"> 404 </a></li>
+              <li class="nav-item"> <a class="nav-link" href="pages/samples/error-500.html"> 500 </a></li>
+            </ul>
+          </div>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" data-toggle="collapse" href="#impuestos" aria-expanded="false" aria-controls="impuestos">
+            <i class="icon-grid-2 menu-icon menu-icon"></i>
+            <span class="menu-title">Impuestos</span>
+            <i class="menu-arrow"></i>
+          </a>
+          <div class="collapse" id="impuestos">
+            <ul class="nav flex-column sub-menu">
+              <li class="nav-item"> <a class="nav-link" href="pages/samples/error-404.html"> 404 </a></li>
+              <li class="nav-item"> <a class="nav-link" href="pages/samples/error-500.html"> 500 </a></li>
+            </ul>
+          </div>
+        </li>
+        
+      </ul>
+    </nav>
+    
+  </div>   
+  <!-- page-body-wrapper ends -->
+</div>
+<!-- container-scroller -->
